e48d98c85e0ebccadff7f8ef7f5101195190c9e8
Michał Skoczek <mwskoczek@gmail.com>
2020-04-16 22:42:42 +0200

    Updates

---
+++ precache-manifest.8d1143f08d681a28408f3fb8ead32be7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af882af2dd3db61f99098fe6ae3e5993",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "37496ffe2aac1781ecd9",
+    "url": "/pokedex/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "10558c3162cc0752bfed",
+    "url": "/pokedex/static/css/main.847948bc.chunk.css"
+  },
+  {
+    "revision": "37496ffe2aac1781ecd9",
+    "url": "/pokedex/static/js/2.338aea07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.338aea07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10558c3162cc0752bfed",
+    "url": "/pokedex/static/js/main.10386b95.chunk.js"
+  },
+  {
+    "revision": "601f8d113175821311bd",
+    "url": "/pokedex/static/js/runtime-main.0ec6b405.js"
+  }
+]);
